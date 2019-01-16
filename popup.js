812af9e9a96631671c4dc6c1340ef62ac33d2a7e
vamakin <vamakin@gmail.com>
2019-01-16 12:56:50 +0300

    заполнение формы

--- popup.js
+++ popup.js
@@ -60,7 +60,7 @@ messageField.onchange = () => {
 
 sendBtn.onclick = function () {
   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, message);
+    chrome.tabs.sendMessage(tabs[0].id, {message});
   });
 };
 
